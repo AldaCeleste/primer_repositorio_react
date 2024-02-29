0e6bbc4e2953c8afd639981571f202f5e1eb46ed
AldaCelste <aceleste.paez46@gmail.com>
2024-02-29 05:11:42 -0300

    proyecto de React con Digital House

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Web from './components/Web'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Web />
+    </>
+  )
+}
+
+export default App
